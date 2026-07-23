0bb9acc8f311e36d033ae5e151ed0d3416f16edb
Karolis Bagdonavičius <karka050505@gmail.com>
2026-07-23 13:47:11 +0300

    backgrounf image test

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
 
             {/* ================= HERO ================= */}
 
-            <section id="home" className="hero" style={{ backgroundImage: "url('/svg/hero-background.svg')" }}>
+            <section id="home" className="hero" style={{ backgroundImage: "url('/images/google misko ritmu logo.svg')" }}>
 
                 <div className="hero-content">
 
