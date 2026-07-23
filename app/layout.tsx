2ff7bfc2213e130b7adfd423e4ffd3bcaa324acb
Karolis Bagdonavičius <karka050505@gmail.com>
2026-07-23 17:54:25 +0300

    flavicon pt3

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "Miško Ritmu",
     description: "Miškininkystės ir konsultacijų paslaugos",
     icons: {
-        icon: "/logo juodas.svg",
+        icon: "/images/logo juodas.svg",
     },
 };
 
