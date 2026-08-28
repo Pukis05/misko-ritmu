8c69e5be366711037bc1adf16db2558eb59be73a
Karolis Bagdonavičius <karka050505@gmail.com>
2026-08-28 14:41:20 +0300

    services change

--- app/page.tsx
+++ app/page.tsx
@@ -60,52 +60,64 @@ export default function Home() {
 
                         <div className="card">
                             <div className="card-body">
-                                <p>Miškotvarkos projektai</p>
+                                <h3>Miškotvarkos projektai</h3>
+                                <p>Parengiame miškotvarkos projektą – pagrindinį dokumentą, be kurio negalima vykdyti kirtimų, miško atkūrimo ir kitos ūkinės veiklos valdoje.</p>
                             </div>
                         </div>
 
                         <div className="card">
                             <div className="card-body">
-                                <p>Miško įveisimo (želdinimo) projektai</p>
+                                <h3>Miško įveisimo (želdinimo) projektai</h3>
+                                <p>Parengiame želdinimo ir žėlimo projektą, reikalingą norint apsodinti žemę mišku ir gauti ES paramą miško įveisimui.</p>
                             </div>
                         </div>
+
                         <div className="card">
                             <div className="card-body">
                                 <h3>Leidimų kirsti mišką išėmimas</h3>
                                 <p>Padedame gauti visus reikiamus leidimus miško kirtimui, tvarkydami dokumentaciją pagal LR miškų įstatymo reikalavimus.</p>
                             </div>
                         </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <p>Biržių atrėžimas pagrindiniams ir tarpiniams kirtimams pagal FSC reikalavimus</p>
-                            </div>
-                        </div>
-                        <div className="card">
-                            <div className="card-body">
-                                <p>Stataus miško tūrio nustatymas vienetiniu medžių matavimo būdu</p>
 
-                            </div>
-                        </div>
                         <div className="card">
                             <div className="card-body">
-                                <p>LR valstybės miškų kadastro tikslinimas</p>
+                                <h3>Biržių atrėžimas pagrindiniams ir tarpiniams kirtimams pagal FSC reikalavimus</h3>
+                                <p>Atribojame kertamą plotą, pažymime kirstinus medžius ir nustatome jų tūrį, laikydamiesi FSC sertifikavimo reikalavimų.</p>
                             </div>
                         </div>
 
                         <div className="card">
                             <div className="card-body">
-                                <p>Miško valdos ribų žymėjimas</p>
+                                <h3>Stataus miško tūrio nustatymas vienetiniu medžių matavimo būdu</h3>
+                                <p>Elektroninėmis žerglėmis ir aukštimačiu išmatuojame kiekvieną medį ir pateikiame tikslius duomenis apie stataus miško tūrį.</p>
                             </div>
                         </div>
 
                         <div className="card">
                             <div className="card-body">
-                                <p>Konsultacijos miškotvarkos ir miškininkystės klausimais</p>
+                                <h3>LR valstybės miškų kadastro tikslinimas</h3>
+                                <p>Parengiame ir pateikiame dokumentus, reikalingus miško valdos duomenims patikslinti valstybės miškų kadastre.</p>
                             </div>
                         </div>
+
                         <div className="card">
                             <div className="card-body">
-                                <p>Pažymos dėl jaunuolynų ugdymo reikalingumo (NMA)</p>
+                                <h3>Miško valdos ribų žymėjimas</h3>
+                                <p>Pagal valdos planą natūroje pažymime miško ribas riboženkliais, taip padėdami išvengti ginčų su kaimynais.</p>
+                            </div>
+                        </div>
+
+                        <div className="card">
+                            <div className="card-body">
+                                <h3>Konsultacijos miškotvarkos ir miškininkystės klausimais</h3>
+                                <p>Konsultuojame miško savininkus dėl valdos priežiūros, kirtimų planavimo ir kitų miškininkystės teisinių reikalavimų.</p>
+                            </div>
+                        </div>
+
+                        <div className="card">
+                            <div className="card-body">
+                                <h3>Pažymos dėl jaunuolynų ugdymo reikalingumo (NMA)</h3>
+                                <p>Parengiame specialisto pažymą dėl jaunuolynų ugdymo reikalingumo – dokumentą, būtiną teikiant paraišką NMA paramai gauti.</p>
                             </div>
                         </div>
 
