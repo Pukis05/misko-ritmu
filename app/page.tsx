41a11f6a9c7f02487d94c656a4ae8d27d401635a
Karolis Bagdonavičius <karka050505@gmail.com>
2026-09-05 12:02:04 +0300

    about change

--- app/page.tsx
+++ app/page.tsx
@@ -137,13 +137,15 @@ export default function Home() {
 
                     <div className="about-content">
                         <p>
-                        Esu įgijęs aukštąjį universitetinį miškininkystės išsilavinimą ir miškininkystės magistro kvalifikacinį laipsnį bei turiu daugiau nei 20 metų profesinę patirtį miškotvarkos, miškų inventorizacijos, miško ūkinių priemonių projektavimo, miškotvarkos duomenų apdorojimo ir VMP rengimo srityse.
+                        Esu įgijęs aukštąjį universitetinį miškininkystės išsilavinimą ir miškininkystės magistro kvalifikacinį laipsnį bei turiu daugiau nei 20 metų profesinę patirtį miškotvarkos, 
+                        miškų inventorizacijos, miško ūkinių priemonių projektavimo, miškotvarkos duomenų apdorojimo ir VMP rengimo srityse.
                         </p>
                         <p>
-                        Sukaupta praktinė patirtis leidžia racionaliai planuoti ir efektyviai atlikti lauko darbus, operatyviai apdoroti inventorizacijos duomenis, atlikti GIS darbus, projektuoti miškotvarkos priemones bei rengti projektinę dokumentaciją.
+                        Sukaupta praktinė patirtis leidžia racionaliai planuoti ir efektyviai atlikti lauko darbus, operatyviai apdoroti inventorizacijos duomenis, atlikti GIS darbus, 
+                        projektuoti miškotvarkos priemones bei rengti projektinę dokumentaciją.
                         </p>
                         <p>
-                        Darbams atlikti turiu reikalingą nuosavą kompiuterinę, matavimo ir kitą techninę įrangą bei darbui su erdviniais ir miškotvarkos duomenimis reikalingas priemones. Dėl to sutarties vykdymui nereikia papildomai įsigyti ar nuomotis pagrindinės darbams atlikti reikalingos techninės įrangos.
+                        Darbams atlikti turiu reikalingą nuosavą kompiuterinę, matavimo ir kitą techninę įrangą bei darbui su erdviniais ir miškotvarkos duomenimis reikalingas priemones.
                         </p>
                     </div>
                         
